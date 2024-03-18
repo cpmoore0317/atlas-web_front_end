62af650415470964bb72fe11afd843a93211a85b
cpmoore0317 <kingpkayy@gmail.com>
2024-03-18 15:07:08 -0500

    Reusing countPrimeNumbers and executing it 100 times logging the time it takes

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,38 @@
+function countPrimeNumbers() {
+    let count = 0;
+
+    // Function to check if a number is prime
+    function isPrime(num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+
+        if (num % 2 === 0 || num % 3 === 0) return false;
+
+        let i = 5;
+        while(i * i <= num) {
+            if (num % i === 0 || num % (i + 2) === 0) return false;
+            i += 6;
+        }
+
+        return true;
+    }
+
+    // Count prime numbers from 2 to 100
+    for (let i = 2; i <= 10; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// Measure execution time
+const iterations = 100;
+const startTime = performance.now();
+for (let i = 0; i < iterations; i++) {
+    countPrimeNumbers();
+}
+const endTime = performance.now();
+
+console.log(`Execution time of calculating prime numbers ${iterations} times was ${endTime - startTime} milliseconds.`);
