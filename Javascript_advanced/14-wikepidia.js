6d217eefb7af4917fe23a36d40c670f124ca1916
cpmoore0317 <kingpkayy@gmail.com>
2024-03-19 09:26:55 -0500

    Final push for task 14

---
+++ Javascript_advanced/14-wikepidia.js
@@ -0,0 +1,30 @@
+function createElement(data) {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = data;
+  document.body.appendChild(paragraph);
+}
+
+function queryWikipedia(callback) {
+  const xhr = new XMLHttpRequest();
+  const url =
+    "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*";
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        const pages = response.query.pages;
+        const pageId = Object.keys(pages)[0];
+        const extract = pages[pageId].extract;
+        callback(extract);
+      } else {
+        console.error("Error fetching data from Wikipedia:", xhr.status);
+      }
+    }
+  };
+
+  xhr.open("GET", url);
+  xhr.send();
+}
+
+queryWikipedia(createElement);
